ec1e16910ef0fb4bdaff77f401063bfd6f0d07b5
André Venâncio <noreply@andrevenancio.com>
2018-07-06 22:01:08 +0100

    logs

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const { PATH_DIST } = require('../webpack/webpack.config');
 const SEARCH = require('./api/search.js');
 
+const fs = require('fs');
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -27,6 +29,13 @@ app.get('/andre', (req, res) => {
 app.use(express.static('./build'));
 app.get('*', (req, res) => {
     console.log('log', req, res);
+
+    console.log('amazing');
+    fs.readdir(PATH_DIST, (err, files) => {
+      files.forEach((file) => {
+        console.log(file);
+      });
+  });
     res.sendFile('index.html', {
         root: path.join(process.cwd(), 'build'),
     });
